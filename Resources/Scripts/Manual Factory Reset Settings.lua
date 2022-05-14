9e8a9114b81eb5ea64337ae366f840cb1b4b0a39
ApfelBube <105540798+ApfelBube@users.noreply.github.com>
2022-05-14 17:04:50 +0200

    Update Manual Factory Reset Settings.lua

--- Resources/Scripts/Manual Factory Reset Settings.lua
+++ Resources/Scripts/Manual Factory Reset Settings.lua
@@ -3,11 +3,11 @@ getgenv().Aimbot.Settings = {
     SaveSettings = true, -- Re-execute upon changing
     ReloadOnTeleport = true,
     Enabled = true,
-    TeamCheck = false,
+    TeamCheck = true,
     AliveCheck = true,
     WallCheck = false, -- Laggy
     Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
-    TriggerKey = "MouseButton2",
+    TriggerKey = "N",
     Toggle = false,
     LockPart = "Head" -- Body part to lock on
 }
@@ -16,10 +16,10 @@ getgenv().Aimbot.FOVSettings = {
     Enabled = true,
     Visible = true,
     Amount = 90,
-    Color = "255, 255, 255",
-    LockedColor = "255, 70, 70",
+    Color = "106, 13, 173",
+    LockedColor = "0, 0, 255",
     Transparency = 0.5,
     Sides = 60,
-    Thickness = 1,
+    Thickness = 2,
     Filled = false
 }
